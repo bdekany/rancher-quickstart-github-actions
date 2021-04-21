c961a8e21b022d69e3815fd8118e1573ab74169a
Brice DEKANY <bdekany@users.noreply.github.com>
2021-04-21 18:46:17 +0000

    Create azureblog.tf

---
+++ azureblog.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "var.azure_rg"
+    storage_account_name  = "var.azure_sto"
+    container_name        = "tstate"
+    key                   = "terraform.tfstate"
+  }
+}
