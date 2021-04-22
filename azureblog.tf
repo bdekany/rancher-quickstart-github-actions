bd840730550ab0902db06e4bed03ba22ece3008e
Brice DEKANY <bdekany@users.noreply.github.com>
2021-04-22 13:59:48 +0200

    Update azureblog.tf
    lookup var.azure_sto.blob.core.windows.net on 127.0.0.53:53: no such host

--- azureblog.tf
+++ azureblog.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "var.azure_rg"
-    storage_account_name  = "var.azure_sto"
+    resource_group_name   = var.azure_rg
+    storage_account_name  = var.azure_sto
     container_name        = "tstate"
     key                   = "terraform.tfstate"
   }
